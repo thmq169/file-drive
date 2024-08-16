b84677fef2db6c17eb0083baf29e7c985c3ad9f7
thmq169 <tohoangminhquan.dp@gmail.com>
2024-08-16 19:01:51 +0700

    feat: add source code

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://diverse-silkworm-72.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "scrupulous-setter-828.convex.cloud",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
